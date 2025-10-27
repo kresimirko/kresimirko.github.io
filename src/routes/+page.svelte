ce26c35b9df762c53b1683cb3e2c58710d6db83c
Luka K. Mihovilović <lukak.business@gmail.com>
2025-10-27 20:44:50 +0100

    Change font import from CSS to HTML, change main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,9 +21,10 @@
 <hr />
 
 <div style="font-size: small;">
-	this site uses the <a href="https://catppuccin.com/palette/">catppuccin</a> color palette
-	&bullet; <a href="#" onclick={toggleTheme}>switch themes</a>
-	&bullet; <a href="https://github.com/kresimirko/kresimirko.github.io">view this site's source</a>
+	<p>
+		this site uses the <a href="https://catppuccin.com/palette/">catppuccin</a> color palette
+		&bullet; <a href="#" onclick={toggleTheme}>switch themes</a>
+		&bullet; <a href="https://github.com/kresimirko/kresimirko.github.io">view this site's source</a>
+	</p>
+	<p>&copy; kresimirko 2025</p>
 </div>
-
-<p>&copy; kresimirko 2025</p>
