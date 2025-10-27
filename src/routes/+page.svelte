c1265166f9e0f96b004f2681ea62b2231a8ded73
Luka K. Mihovilović <lukak.business@gmail.com>
2025-10-27 22:03:02 +0100

    Cooler header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,18 +12,21 @@
 	<title>kresimirko's site</title>
 </svelte:head>
 
+<div id="bigDisclaimer">
+	<p>[temporary design - <b>NOT SUITABLE FOR MOBILE DEVICES</b>]</p>
+</div>
+
 <div id="header">
 	<img id="pfp" alt="GitHub profile picture - my pet dog, Gina" src="https://avatars.githubusercontent.com/u/104278083">
 	<div id="headerText">
 		<h1>kresimirko</h1>
 		<h2>Luka Krešimir Mihovilović</h2>
 	</div>
+	<div id="headerBg"></div>
 </div>
 
 <div id="main">
-	<p>[temporary website - <b>NOT SUITABLE FOR MOBILE DEVICES</b>]</p>
-
-	<h2>Works</h2>
+	<h1>Works</h1>
 
 	<p><a href="./kikipnt/">kikipnt</a></p>
 	<p><a href="./digitalizacija-tv/">Digitalizacija televizije</a></p>
@@ -42,12 +45,34 @@
 </div>
 
 <style>
+	#bigDisclaimer {
+		padding: 0 1rem 0 1rem;
+		display: flex;
+		justify-content: center;
+		background-color: var(--error);
+		color: var(--base);
+	}
+
 	#header {
-		background-color: var(--mantle);
+		background-color: rgba(24, 24, 37, 0.75);
 		padding: 6rem 10rem 2rem 10rem;
 		display: flex;
 		flex-direction: row;
 		gap: 3rem;
+		position: relative;
+	}
+
+	#headerBg {
+		background-image: url("https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070"); /* scaled-down version */
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: 0 30%;
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: -1;
 	}
 
 	#headerText {
@@ -77,6 +102,6 @@
 	}
 
 	#main {
-		padding: 1rem 10rem 3rem 10rem;
+		padding: 3rem 10rem 3rem 10rem;
 	}
 </style>
