7f40529e31d45a5f80b2b4554130e7a081113008
Luka K. Mihovilović <lukak.business@gmail.com>
2025-10-27 20:41:17 +0100

    Font change, theme switcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	onMount(() => {
+		function setTheme(themeName: string) {
+			localStorage.setItem('theme', themeName);
+			document.documentElement.className = themeName;
+		}
+		// sanitization, I guess
+		switch (localStorage.getItem('theme')) {
+			case 'theme-light':
+				setTheme('theme-light');
+				break;
+			// I prefer dark tyvm
+			case 'theme-dark':
+			default:
+				setTheme('theme-dark');
+				break;
+		}
+	});
 </script>
 
 <svelte:head>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	function setTheme(themeName: string) {
+		localStorage.setItem('theme', themeName);
+		document.documentElement.className = themeName;
+	}
+	function toggleTheme() {
+		setTheme(localStorage.getItem('theme') === 'theme-dark' ? 'theme-light' : 'theme-dark');
+	}
+</script>
+
+<h1>kresimirko.github.io</h1>
+
+<p>[temporary website]</p>
+
+<h2>Works</h2>
+
+<p><a href="./kikipnt/">kikipnt</a></p>
+<p><a href="./digitalizacija-tv/">Digitalizacija televizije</a></p>
+<p><a href="./karta-trogira/">Karta gradske jezgre Trogira</a></p>
+
+<hr />
+
+<div style="font-size: small;">
+	this site uses the <a href="https://catppuccin.com/palette/">catppuccin</a> color palette
+	&bullet; <a href="#" onclick={toggleTheme}>switch themes</a>
+	&bullet; <a href="https://github.com/kresimirko/kresimirko.github.io">view this site's source</a>
+</div>
+
+<p>&copy; kresimirko 2025</p>
